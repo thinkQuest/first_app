a74a99e17ef2f2db08673441002d8539dfd1c07a
thinkQuest <srikanthkambhampati@hotmail.com>
2013-04-16 12:13:04 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4ca04e356aa15e204970ceda566179031cf506fae5a92286498a6bd965772386fbf627a003c5fb3bb01f6a20a83eefd52cb32f5fbb1708ba7e64d6760a392b2b'
